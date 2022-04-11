26f73eb4d6db162ebc2c49cd5e12e8e6840d6b1a
Dan Bowen <steamfire@gmail.com>
2022-04-10 21:31:15 -0700

    Working BH1750 light sensor simple demo

---
+++ test2bh1750/test2bh1750.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <BH1750.h>
+
+
+#define PIN_I2C_POWER 7
+
+BH1750 lightMeter;
+
+void setup(){
+
+    // turn on the I2C power by setting pin to opposite of 'rest state'
+  pinMode(PIN_I2C_POWER, INPUT);
+  delay(1);
+  bool polarity = digitalRead(PIN_I2C_POWER);
+  pinMode(PIN_I2C_POWER, OUTPUT);
+  digitalWrite(PIN_I2C_POWER, !polarity);
+  
+
+  Serial.begin(9600);
+  delay(2000);
+
+  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  // On esp8266 devices you can select SCL and SDA pins using Wire.begin(D4, D3);
+  Wire.begin();
+
+  lightMeter.begin();
+  Serial.println(F("BH1750 Test"));
+
+}
+
+void loop() {
+
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(1000);
+
+}
