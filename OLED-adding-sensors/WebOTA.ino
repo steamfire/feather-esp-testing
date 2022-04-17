7c637c7726f7632946fff61937cf3193038f8541
Dan Bowen <steamfire@gmail.com>
2022-04-17 01:31:08 -0700

    Added multi-wifi networks support

--- OLED-adding-sensors/WebOTA.ino
+++ OLED-adding-sensors/WebOTA.ino
@@ -1,29 +1,35 @@
 #include <WiFi.h>
+#include <WiFiMulti.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncElegantOTA.h>
 
 
-const char* ssid = "Noisebridge";
-const char* password = "noisebridge";
 
+WiFiMulti wifiMulti;
 AsyncWebServer server(80);
+
+string hello =
 
 void netStart(void) {
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+   wifiMulti.addAP("villa", "puttanesca");
+   wifiMulti.addAP("Noisebridge", "noisebridge");
+   
   Serial.println("");
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+
+    Serial.println("Connecting Wifi...");
+    if(wifiMulti.run() == WL_CONNECTED) {
+        Serial.println("");
+        Serial.print("WiFi connected: ");
+        Serial.println(WiFi.SSID());
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+        else {
+        Serial.println("Wifi failed to connect.\n");
+    }
+    
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(200, "text/plain", "Mars yet?.");
